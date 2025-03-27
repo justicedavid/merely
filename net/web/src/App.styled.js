9d4bd3c62a838d26e3228312083fa66713c681f3
justicedavid <justicedavid999@yahoo.com>
2025-03-27 23:42:38 +0000

    Fix configuration - enhancing logs

--- net/web/src/App.styled.js
+++ net/web/src/App.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const AppWrapper = styled.div`
+export let AppWrapper = styled.div`
   position: absolute;
   width: 100vw;
   height: calc(100vh - calc(100vh - 100%));
