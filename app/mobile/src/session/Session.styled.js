8cb47c6a1c4d4eeb289f11d86b123c30ea88ea33
justicedavid <justicedavid999@yahoo.com>
2025-04-02 21:31:01 +0000

    Change dependencies - fixing typo

--- app/mobile/src/session/Session.styled.js
+++ app/mobile/src/session/Session.styled.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   body: {
     width: '100%',
     height: '100%',
