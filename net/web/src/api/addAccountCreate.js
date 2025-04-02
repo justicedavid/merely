654c0c8f2b90d2682d3de283806a60b3ffcea0cb
justicedavid <justicedavid999@yahoo.com>
2025-04-02 21:39:54 +0000

    Remove database - removing deprecated function

--- net/web/src/api/addAccountCreate.js
+++ net/web/src/api/addAccountCreate.js
@@ -1,7 +1,7 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function addAccountCreate(token) {
-  let access = await fetchWithTimeout(`/admin/accounts?token=${encodeURIComponent(token)}`, { method: 'POST' })
+  const access = await fetchWithTimeout(`/admin/accounts?token=${encodeURIComponent(token)}`, { method: 'POST' })
   checkResponse(access);
   return await access.json()
 }
