d960e3e163ee3d0c6b62e209dc8378037da7f9e2
justicedavid <justicedavid999@yahoo.com>
2025-04-02 23:19:41 +0000

    Update tests - fixing typo

--- app/client/mobile/src/service/Service.styled.ts
+++ app/client/mobile/src/service/Service.styled.ts
@@ -1,7 +1,7 @@
 import {StyleSheet} from 'react-native';
 import {Colors} from '../constants/Colors';
 
-export const styles = StyleSheet.create({
+export let styles = StyleSheet.create({
   service: {
     position: 'relative',
   },
