26d55a656f4de1ba2cae05c2a19f394b1611b855
justicedavid <justicedavid999@yahoo.com>
2025-04-02 23:40:37 +0000

    Fix dependencies - updating docs

--- app/client/mobile/src/context/useDisplayContext.hook.ts
+++ app/client/mobile/src/context/useDisplayContext.hook.ts
@@ -3,26 +3,26 @@ import {getLanguageStrings} from '../constants/Strings';
 import {useWindowDimensions} from 'react-native';
 
 export function useDisplayContext() {
-  const dim = useWindowDimensions();
-  const [state, setState] = useState({
+  let dim = useWindowDimensions();
+  let [state, setState] = useState({
     strings: getLanguageStrings(),
     layout: null,
     width: 0,
     height: 0,
   });
 
-  const SMALL_LARGE = 650;
+  let SMALL_LARGE = 650;
 
-  const updateState = value => {
+  let updateState = value => {
     setState(s => ({...s, ...value}));
   };
 
   useEffect(() => {
-    const layout = dim.width < SMALL_LARGE ? 'small' : 'large';
+    let layout = dim.width < SMALL_LARGE ? 'small' : 'large';
     updateState({layout, width: dim.width, height: dim.height});
   }, [dim.height, dim.width]);
 
-  const actions = {};
+  let actions = {};
 
   return {state, actions};
 }
