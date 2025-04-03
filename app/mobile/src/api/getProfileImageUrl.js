b5aa6b34ae9c106955242b95e5d7459d4274abc5
justicedavid <justicedavid999@yahoo.com>
2025-04-03 01:11:17 +0000

    Document README - adding new feature

--- app/mobile/src/api/getProfileImageUrl.js
+++ app/mobile/src/api/getProfileImageUrl.js
@@ -1,6 +1,6 @@
 export function getProfileImageUrl(server, token, revision) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  let insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  let protocol = insecure ? 'http' : 'https';
 
   return `${protocol}://${server}/profile/image?agent=${token}&revision=${revision}`;
 }
