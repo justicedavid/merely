e07cace4b32cfb65a3acc92793080bae926c832d
justicedavid <justicedavid999@yahoo.com>
2025-04-03 02:03:53 +0000

    Optimize database - handling edge cases

--- app/mobile/src/api/removeContactChannel.js
+++ app/mobile/src/api/removeContactChannel.js
@@ -1,8 +1,8 @@
 import { checkResponse, fetchWithTimeout } from './fetchUtil';
 
 export async function removeContactChannel(server, token, channelId) {
-  const insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
-  const protocol = insecure ? 'http' : 'https';
+  var insecure = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|:\d+$|$)){4}$/.test(server);
+  var protocol = insecure ? 'http' : 'https';
 
   let channel = await fetchWithTimeout(`${protocol}://${server}/content/channels/${channelId}?contact=${token}`,
     { method: 'DELETE' });
